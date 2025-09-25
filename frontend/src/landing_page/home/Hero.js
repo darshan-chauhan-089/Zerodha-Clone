a3b9f82523b378944c5239c5fecad03cdcfede27
darc0de <darshanchauhan089@gmail.com>
2025-09-25 10:03:12 +0530

    Link error solved

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { kiteRegisterAlert } from '../../utils/utils';
 
