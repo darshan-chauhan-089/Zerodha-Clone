b3bb4c6424ccc5009d12c640597d8346d451b1bd
darc0de <darshanchauhan089@gmail.com>
2025-09-27 11:38:18 +0530

    all redirects links changes loaclly to remotely for production 1.0

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
       <div className={`container mb-5 mt-3 ${user ? "pb-4" : ""}`}>
         <div className='row text-center'>
             <div className='hero-img-container d-flex justify-content-center mb-4'>
-                <Link className='' to={`${process.env.REACT_APP_API_URL}/${user?.id}`} onClick={(e) => kiteRegisterAlert(e, user)}>
+                <Link className='' to={`${process.env.REACT_APP_ZERODHA_DASHBOARD}/${user?.id}`} onClick={(e) => kiteRegisterAlert(e, user)}>
                   <img className='' src='media/images/homeHero.png' alt='Hero_Image' style={{ width: "80%" }} ></img>
                 </Link>
             </div>
